8d456d851e6301b900ebc7f601ce3a6ff5213cc1
williambulin <williambulin01@gmail.com>
2020-11-08 19:15:28 +0100

    change: model matrix

--- engine/source/Components/Transform.hpp
+++ engine/source/Components/Transform.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Extension/Libraries/glm.hpp"
+#include "Maths/Math.hpp"
 
 namespace Components {
   class Transform;
@@ -13,13 +14,22 @@ public:
   glm::vec3 scale{1.0f};
 
 public:
-  [[nodiscard]] inline auto toModelMatrix() const -> glm::mat4 {
-    glm::mat4 mat{1.0f};
-    mat = glm::scale(mat, scale);
-    mat = glm::rotate(mat, rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
-    mat = glm::rotate(mat, rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
-    mat = glm::rotate(mat, rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
-    mat = glm::translate(mat, position);
+  [[nodiscard]] inline auto toModelMatrix() const -> ml::mat4 {
+    ml::mat4 mat{1.0f};
+    mat = mat.scale(scale.x, scale.y, scale.z);
+
+// Can't rotate?
+#error It seems like we can only rotate with a matrix and not with individual components, check below
+
+    mat = mat.translate(ml::vec3{position.x, position.y, position.z});
     return mat;
+
+    // glm::mat4 mat{1.0f};
+    // mat = glm::scale(mat, scale);
+    // mat = glm::rotate(mat, rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+    // mat = glm::rotate(mat, rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+    // mat = glm::rotate(mat, rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+    // mat = glm::translate(mat, position);
+    // return mat;
   }
 };
