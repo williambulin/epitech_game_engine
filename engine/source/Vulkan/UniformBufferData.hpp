504dec8719502cd89eccb45d1a3daacf14986869
OrangeCat <no>
2020-11-08 18:39:40 +0100

    add: lookat replacement

--- engine/source/Vulkan/UniformBufferData.hpp
+++ engine/source/Vulkan/UniformBufferData.hpp
@@ -9,6 +9,6 @@ namespace Vulkan {
 
 struct Vulkan::UniformBufferData {
   alignas(16) glm::mat4 model;
-  alignas(16) glm::mat4 view;
+  alignas(16) Matrix4<float> view;
   alignas(16) Matrix4<float> projection;
 };
