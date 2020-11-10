971dbd5c484c06ae805a4b393c293bc46b01e004
Aurelien Foucault <aurelien.foucault@epitech.eu>
2020-11-10 18:00:07 +0100

    Add start of a heck with obb

--- engine/source/Physics/Shapes/OBB.cpp
+++ engine/source/Physics/Shapes/OBB.cpp
@@ -25,8 +25,53 @@ auto OBB::getPoints(const ml::mat4 &transform, bool forceInvalidate) -> std::vec
 
   m_pointsCache  = points;
   m_oldTransform = transform;
+  m_edges        = {
+  std::array<ml::vec3, 2>{m_pointsCache[2], m_pointsCache[3]},
+  std::array<ml::vec3, 2>{m_pointsCache[3], m_pointsCache[4]},
+  std::array<ml::vec3, 2>{m_pointsCache[4], m_pointsCache[7]},
+  std::array<ml::vec3, 2>{m_pointsCache[2], m_pointsCache[7]},
+  std::array<ml::vec3, 2>{m_pointsCache[6], m_pointsCache[5]},
+  std::array<ml::vec3, 2>{m_pointsCache[5], m_pointsCache[0]},
+  std::array<ml::vec3, 2>{m_pointsCache[0], m_pointsCache[1]},
+  std::array<ml::vec3, 2>{m_pointsCache[1], m_pointsCache[6]},
+  std::array<ml::vec3, 2>{m_pointsCache[4], m_pointsCache[5]},
+  std::array<ml::vec3, 2>{m_pointsCache[6], m_pointsCache[7]},
+  std::array<ml::vec3, 2>{m_pointsCache[0], m_pointsCache[3]},
+  std::array<ml::vec3, 2>{m_pointsCache[2], m_pointsCache[1]},
+  };
+
+  m_faces = {
+  std::tuple<std::array<int, 4>, std::array<int, 4>, ml::vec3>({0, 1, 2, 3}, {2, 3, 4, 7}, ml::vec3(0.0f, 0.0f, 0.0f)),
+  std::tuple<std::array<int, 4>, std::array<int, 4>, ml::vec3>({4, 5, 6, 7}, {6, 5, 0, 1}, ml::vec3(0.0f, 0.0f, 0.0f)),
+  std::tuple<std::array<int, 4>, std::array<int, 4>, ml::vec3>({2, 8, 4, 9}, {7, 4, 5, 6}, ml::vec3(0.0f, 0.0f, 0.0f)),
+  std::tuple<std::array<int, 4>, std::array<int, 4>, ml::vec3>({6, 10, 0, 11}, {1, 0, 3, 2}, ml::vec3(0.0f, 0.0f, 0.0f)),
+  std::tuple<std::array<int, 4>, std::array<int, 4>, ml::vec3>({1, 10, 5, 8}, {4, 3, 0, 5}, ml::vec3(0.0f, 0.0f, 0.0f)),
+  std::tuple<std::array<int, 4>, std::array<int, 4>, ml::vec3>({3, 9, 7, 11}, {2, 7, 6, 1}, ml::vec3(0.0f, 0.0f, 0.0f)),
+  };
+
+  for (std::tuple<std::array<int, 4>, std::array<int, 4>, ml::vec3> &tup : m_faces) {
+    std::array<int, 4> vertices = std::get<OBB::VERTICES>(tup);
+    Vector3<float>     a{points[vertices[1]] - points[vertices[0]]};
+    Vector3<float>     normal = a.cross(points[vertices[2]] - points[vertices[0]]);
+    normal.normalize();
+    std::get<OBB::NORMAL>(tup) = normal;
+  }
+
   return points;
 }  // Called by collide(...)
+
+auto OBB::getSupport(const ml::vec3 &axis) const noexcept -> ml::vec3 {
+  float    distance = -FLT_MAX;
+  Vector3f furthest = ml::vec3(0.0f, 0.0f, 0.0f);
+  for (int i = 0; i < m_pointsCache.size(); i++) {
+    float projection = m_pointsCache[i].dot(axis);
+    if (projection > distance) {
+      distance = projection;
+      furthest = m_pointsCache[i];
+    }
+  }
+  return furthest;
+}
 
 void OBB::setMin(const ml::vec3 &min) noexcept {
   m_min         = min;
--- engine/source/Physics/Shapes/OBB.hpp
+++ engine/source/Physics/Shapes/OBB.hpp
@@ -6,6 +6,11 @@
 
 class OBB final : public ICollisionShape {
 public:
+  enum TupleFaces {
+    EDGES,
+    VERTICES,
+    NORMAL
+  };
   explicit OBB(const ml::vec3 &min, const ml::vec3 &max) noexcept;
 
   explicit OBB(const OBB &second) noexcept;
@@ -16,14 +21,16 @@ public:
   [[nodiscard]] auto getMin() const noexcept -> ml::vec3;
   void               setMax(const ml::vec3 &max) noexcept;
   [[nodiscard]] auto getMax() const noexcept -> ml::vec3;
-
+  [[nodiscard]] auto getSupport(const ml::vec3 &axis) const noexcept -> ml::vec3;
   [[nodiscard]] bool operator==(const OBB &second) const noexcept;
 
   ml::vec3 getLocalPosition() override;
-
+  std::vector<std::array<ml::vec3, 2>> m_edges{};
+  std::vector<std::tuple<std::array<int, 4>, std::array<int, 4>, ml::vec3>> m_faces{};
 private:
   ml::vec3              m_min{0.0f, 0.0f, 0.0f};
   ml::vec3              m_max{0.0f, 0.0f, 0.0f};
+
   ml::mat4              m_oldTransform{};
   std::vector<ml::vec3> m_pointsCache{};
 };
