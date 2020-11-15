48a62d0b8ffebaef16a29fe89a11eb6c8788afee
moi <3032fmartinage@gmail.com>
2020-11-15 22:42:28 +0100

    a

--- engine/source/Systems/Gravity.cpp
+++ engine/source/Systems/Gravity.cpp
@@ -3,6 +3,18 @@
 void Systems::Gravity::update(float dt, std::uint64_t) {
   auto &entities{getItems()};
   for (auto &&[entity, gravity, physics] : entities)
-    if (!physics.getIsRigid())
-      physics.applyLinearImpulse(ml::vec3{0.0f, -9.81f * dt, 0.0f});
+    if (!physics.getIsRigid()) {
+      auto tmp = physics.getLinearVelocity();
+
+      if (tmp.y > 0) {
+        tmp.y += -9.81f;
+        tmp.y *= .75f;
+      }
+      else if (tmp.y < 0) {
+        tmp.y += -9.81f;
+        tmp.y *= 1.5f;
+      } else
+        tmp.y += -9.81f;
+    }
+      //physics.applyLinearImpulse(ml::vec3{0.0f, -9.81f * dt, 0.0f});
 }
