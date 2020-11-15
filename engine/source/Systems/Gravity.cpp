ad6cb0b7dbd5d789425e131ef0ce62cdc2087b33
Aurelien Foucault <aurelien.foucault@epitech.eu>
2020-11-15 22:22:29 +0100

    Add boolean gravity

--- engine/source/Systems/Gravity.cpp
+++ engine/source/Systems/Gravity.cpp
@@ -3,6 +3,6 @@
 void Systems::Gravity::update(float dt, std::uint64_t) {
   auto &entities{getItems()};
   for (auto &&[entity, gravity, physics] : entities)
-    if (!physics.getIsRigid())
+    if (!physics.getIsRigid() && gravity.hasGravity)
       physics.applyLinearImpulse(ml::vec3{0.0f, -9.81f * dt, 0.0f});
 }
